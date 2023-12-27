c6b9cfc633ee7d936ef85f19a505426703a9f296
Pavel Davydov <Davydov.works@gmail.com>
2023-12-27 01:12:34 -0500

    Add test UI

--- src/test/java/test/ui/CreditTest.java
+++ src/test/java/test/ui/CreditTest.java
@@ -37,6 +37,49 @@ public class CreditTest {
         assertEquals("APPROVED", actual);
     }
 
+    @Test
+    @DisplayName("Card with DECLINED status")
+    void shouldDeclinePaymentByCard() {
+        var creditPage = page.creditButtonClick();
+        var cardInfo = DataHelper.getValidDataWithDeclinedCard();
+        creditPage.inputData(cardInfo);
+        creditPage.getErrorNotification();
+        var actual = DataBaseHelper.getStatusCreditRequest();
+        assertEquals("DECLINED", actual);
+    }
+
+    @Test
+    @DisplayName("Card with empty fields")
+    void shouldFailValidationCardWithEmptyFields() {
+        var creditPage = page.creditButtonClick();
+        var cardInfo = DataHelper.getCardInfoWithEmptyFields();
+        creditPage.inputData(cardInfo);
+        creditPage.checkingEmptyField();
+    }
+    @Test
+    @DisplayName("Card with empty number")
+    void shouldFailValidationWithEmptyCardNumber() {
+        var creditPage = page.creditButtonClick();
+        var cardInfo = DataHelper.getCardInfoWithEmptyNumber();
+        creditPage.inputData(cardInfo);
+        creditPage.checkingWrongFormat();
+    }
+    @Test
+    @DisplayName("Card with random number")
+    void shouldDeclineTransactionCardWithRandomNumber() {
+        var creditPage = page.creditButtonClick();
+        var cardInfo = DataHelper.getCardInfoWithRandomNumber();
+        creditPage.inputData(cardInfo);
+        creditPage.getErrorNotification();
+    }
+    @Test
+    @DisplayName("Card with 15 numbers")
+    void shouldFailValidationWithCard15Numbers() {
+        var creditPage = page.creditButtonClick();
+        var cardInfo = DataHelper.getCardInfoWith15();
+        creditPage.inputData(cardInfo);
+        creditPage.checkingWrongFormat();
+    }
 
 
 }
